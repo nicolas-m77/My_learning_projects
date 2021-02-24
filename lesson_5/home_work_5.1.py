796e316cadf3959dcd2780d6c6f3e354c2318091
nicolas-m77 <98290943@mail.ru>
2021-02-24 21:03:04 +0500

    [upd] четвертая домашняя работа

--- lesson_5/home_work_5.1.py
+++ lesson_5/home_work_5.1.py
@@ -2,3 +2,11 @@
 Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+print('Введите данные построчно: ')
+with open('new.txt', 'w', encoding='UTF-8') as f:
+    while True:
+        my_string = input()
+        if len(my_string) == 0:
+            break
+        my_string = my_string + '\n'
+        f.write(my_string)
