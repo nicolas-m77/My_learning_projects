c91eb6e472d50be238894661b5d3b297262ff2ad
nicolas-m77 <98290943@mail.ru>
2021-02-27 01:47:25 +0500

    [upd] пятая домашняя работа

--- lesson_5/home_work_5.4.py
+++ lesson_5/home_work_5.4.py
@@ -8,3 +8,22 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+n = 0
+with open('for_5.4.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        my_string = line.split()
+        if my_string[0] == 'One':
+            my_string[0] = 'Один'
+        elif my_string[0] == 'Two':
+            my_string[0] = 'Два'
+        elif my_string[0] == 'Three':
+            my_string[0] = 'Три'
+        elif my_string[0] == 'Four':
+            my_string[0] = 'Четыре'
+        new_string = " ".join(my_string)
+        with open('new_for_5.4.txt', 'a', encoding='utf-8') as new_file:
+            new_file.write(new_string)
+            n += 1
+            if n == 4:  # Убираем лишний перенос строк после 4 строки
+                break
+            new_file.write('\n')
